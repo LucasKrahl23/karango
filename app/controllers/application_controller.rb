033a71f63f7942ebced7089ab346430c6c3831ec
Lucas Krahl <luckrahl1996@gmail.com>
2016-09-01 22:50:25 -0300

    Rota para logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  def after_sign_in_path_for(resource)
+  	protect_from_forgery with: :exception
+  	def after_sign_in_path_for(resource)
       veiculos_url
+	end
+	def after_sign_out_path_for(resource)
+      new_usuario_session_path
 	end
 end
