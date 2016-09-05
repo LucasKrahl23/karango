03b08caf197aefa6589e9c50621e701ea086b645
Lucas Krahl <luckrahl1996@gmail.com>
2016-09-05 19:44:03 -0300

    #15 Layout

--- db/schema.rb
+++ db/schema.rb
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 20160820025725) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "token"
