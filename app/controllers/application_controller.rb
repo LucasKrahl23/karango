a81c039e10462f2f84e7be50cb9d5971a490a86c
Lucas Krahl <luckrahl1996@gmail.com>
2016-09-05 20:39:42 -0300

    #15 Cadastro de usuários

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
   	protect_from_forgery with: :exception
   	def after_sign_in_path_for(resource)
       veiculos_url
@@ -6,4 +7,10 @@ class ApplicationController < ActionController::Base
 	def after_sign_out_path_for(resource)
       new_usuario_session_path
 	end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nome])
+  end
 end
