ec087fd375833e1e7fa0ff7cde1b30b24c3c84b0
Lucas Krahl <luckrahl1996@gmail.com>
2016-09-05 21:42:35 -0300

    #16 Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
+	layout :layout_by_resource
+  before_action :configure_permitted_parameters, if: :devise_controller?
   	protect_from_forgery with: :exception
   	def after_sign_in_path_for(resource)
       veiculos_url
@@ -12,5 +13,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :perfisusuario_id])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nome, :perfisusuario_id])
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
   end
 end
